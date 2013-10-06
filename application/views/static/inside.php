9ef759a586f7ec60bb79490ebcb7c50e80dfcbf1
Mick <mmailm@mail.ru>
2013-10-07 03:18:27 +0900

    Fixed inside page

--- application/views/static/inside.php
+++ application/views/static/inside.php
@@ -1,7 +1,24 @@
 <html>
 <head>
-<base href="<?php echo base_url(); ?>/www/" />
+<base href="<?php echo base_url(); ?>www/" />
+<style>
+	body {
+		margin:0;
+		padding:0;
+		background-color:black;
+	}
+	img.center {
+		position:absolute;
+   		top:50%;
+   		left:50%;
+		-webkit-transform: translateY(-50%) translateX(-50%); 
+		-moz-transform: translateY(-50%) translateX(-50%); 
+		-ms-transform: translateY(-50%) translateX(-50%); 
+		transform: translateY(-50%) translateX(-50%);
+	}
+</style>
 </head>
-<body style='background-repeat: no-repeat; background-position: center; background-color:black; background-image: url("styles/images/inside_3.gif")'>
+<body class="inside">
+	<img class="center" src = "images/inside_3.gif"/>
 </body>	
 </html>
