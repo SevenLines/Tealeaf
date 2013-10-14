8ff4feb7f2db62f596685637ef964c3c7328b311
SevenLines <mmailm@mail.ru>
2013-10-15 00:19:50 +0900

    Added new pink-orange design, add books and new dump

--- application/views/article.php
+++ application/views/article.php
@@ -1,7 +1,9 @@
 <?php echo PHP_EOL;?>
 <div class="content">
+<?php if( isset($title) && $title !== '' ) { ?>
 <h1><?php echo $title;?></h1>
 <hr>
+<?php } ?>
 <div class="plain_text">
 <?php if (isset($text)) echo $text; ?>
 </div>
--- application/views/lessons/article.php
+++ application/views/lessons/article.php
@@ -1,8 +1,10 @@
 <?php echo PHP_EOL;?>
 <div class="content">
+<?php if (isset($title) && $title !== '') { ?>
 <hr>
 <h1><?php echo $title;?></h1>
 <hr>
+<?php } ?>
 <div class="plain_text">
 <?php if (isset($articles_info)) echo $articles_info->text; ?>
 </div>
