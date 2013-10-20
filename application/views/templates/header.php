d29f073c07cc45cfb08fb80d214e37650225f8e0
Mick <mmailm@mail.ru>
2013-10-21 02:01:13 +0900

    some quick cleaning

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
 
 <link rel="stylesheet" type="text/css" href="styles/main.css">
 <link rel="stylesheet" type="text/css" href="styles/menu.css">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
 <script type="text/javascript" src="scripts/jquery.min.js"></script>
 <script type="text/javascript" src="scripts/main.js"></script>
 <meta charset="utf-8">
