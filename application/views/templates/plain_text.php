6562791a5d093d8ecd5cf180839e3953d3ad422b
Mick <mmailm@mail.ru>
2014-01-18 23:16:59 +0900

    добавил иконки для файлов, небольшие фиксы

--- application/views/templates/plain_text.php
+++ application/views/templates/plain_text.php
@@ -1,5 +1,19 @@
-<?php 
-if (isset($text) && $text !== '' ) {
-	echo $text;
-} 
+<?php
+
+namespace Netcarver\Textile;
+
+include_once 'Textile/Parser.php';
+include_once 'Textile/DataBag.php';
+include_once 'Textile/Tag.php';
+
 ?>
+<div class="plain_text">
+<?php 
+	
+        $parser = new Parser(); 
+	if (isset($text))  {
+		$output = $parser->textileThis($text);
+		echo $output;
+	} 
+?>
+</div>
